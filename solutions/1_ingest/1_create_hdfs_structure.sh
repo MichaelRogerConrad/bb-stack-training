2dbfe4283f718431bb4cd0b3b5858dace4d27cf3
MichaelRogerConrad <33132415+MichaelRogerConrad@users.noreply.github.com>
2017-10-26 20:05:16 +0200

    Delete 1_create_hdfs_structure.sh

--- solutions/1_ingest/1_create_hdfs_structure.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Creating the directory structure on HDFS"
-hadoop fs -mkdir /data
-hadoop fs -mkdir /data/raw
-hadoop fs -mkdir /data/raw/weather
-hadoop fs -mkdir /data/raw/retail
